e3439dbc77254f94c9324d722fb647735a5d5a69
Justin Cathey <95rocketman@gmail.com>
2018-04-11 12:48:50 +0300

    removed initial data inserts

--- src/app.js
+++ src/app.js
@@ -5,25 +5,11 @@ import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense,removeExpense,editExpense} from './actions/expenses';
 import {sortByAmount,sortByDate,setEndDate,setStartDate,setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
-store.subscribe(()=>{
-    const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses,state.filters);
-});
-const expense1 = store.dispatch(addExpense({
-    description:'water bill',amount:100,createdAt:100
-}));
-const expense2 = store.dispatch(addExpense({
-    description:'gas bill',amount:276,createdAt:150
-}));
-const expense3 = store.dispatch(addExpense({
-    description:'electricity bill',amount:400,createdAt:130
-}));
 
 // console.log(store.getState());
 
